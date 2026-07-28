d7234d9bda7650b1c28130131c75d895edcbd610
DURGADURGADURGA <alibabachalischor024@gmail.com>
2026-07-28 17:44:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+APP_NAME = "Family Expense Manager Pro"
+
+VERSION = "1.0.0"
+
+DEFAULT_LANGUAGE = "English"
+
+CURRENCY = "₹"
+
+DATE_FORMAT = "%d-%m-%Y"
+
+THEME = "light"
+
+ADMIN_EMAIL = "admin@family.com"
+
+SUPER_ADMIN = "superadmin@family.com"
