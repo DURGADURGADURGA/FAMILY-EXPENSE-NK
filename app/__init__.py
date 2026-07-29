e24b5afe4107f53240c49efb0b485f3c4a52ffcf
DURGADURGADURGA <alibabachalischor024@gmail.com>
2026-07-29 14:45:29 +0530

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+"""
+=========================================================
+Family Expense Manager Pro
+Application Package
+Version : 1.0.0
+=========================================================
+"""
+
+__version__ = "1.0.0"
+__author__ = "Nilesh Patil"
+__license__ = "Private"
+
+APP_NAME = "Family Expense Manager Pro"
+
+APP_VERSION = "1.0.0"
